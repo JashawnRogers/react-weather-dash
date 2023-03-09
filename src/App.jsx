1b395f8eec669f5053b0e1049722295f891b4a36
Jashawn <rogersjashawn20@gmail.com>
2023-03-08 22:25:40 -0700

    project setup vite + bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Navigation } from './components/Navigation'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Navigation />
+    </div>
+  )
+}
+
+export default App
